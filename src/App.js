73be733db9fcd3c18e20e990f6a6ca96f3dc23b9
Сухомлинов Данил Игоревич <dsuhomlinov@bostonsd.ru>
2021-10-13 00:02:35 +0300

    added router and one exmpl pg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import UpdatesPage from "./Updates";
+import { Router, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from 'history'
+
+const newHistory = createBrowserHistory();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Router history={newHistory}>
+    <Switch>
+      <Route path="/news"><UpdatesPage /></Route>
+      <Route path="/"><p1>This is a Glavnya str</p1></Route>
+    </Switch>
+  </Router>
 }
 
 export default App;
---
+++ src/Updates/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const UpdatesPage = () => {
+    return <div>
+        <p1> This is Updates Page </p1>
+    </div>
+}
+
+export default UpdatesPage
